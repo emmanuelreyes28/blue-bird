a83a463e024e49c719cfff25912481490cfc25b6
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2023-08-01 15:16:32 -0700

    initial commit

---
+++ app/auth/callback/route.ts
@@ -0,0 +1,19 @@
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { NextResponse, type NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+    const requestUrl = new URL(request.url)
+    const code = requestUrl.searchParams.get('code')
+
+    if(code){
+        // use createRouteHandlerClient since this is a route
+        const supabase = createRouteHandlerClient({ cookies })
+        // authenticate user on next.js app and set a cookie meaning they are now signed in
+        await supabase.auth.exchangeCodeForSession(code)
+    }
+
+    // redirect user to landing page 
+    return NextResponse.redirect(requestUrl.origin)
+    
+}
