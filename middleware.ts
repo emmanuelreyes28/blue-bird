d91380b631823084e6bf28dc0da22f9aa8b4b5cc
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2023-08-01 16:39:35 -0700

    add middleware

---
+++ middleware.ts
@@ -0,0 +1,13 @@
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { NextResponse } from "next/server";
+
+import type { NextRequest } from "next/server";
+
+// runs every time before a request is completed
+export async function middleware(req: NextRequest){
+    const res = NextResponse.next();
+    const supabase = createMiddlewareClient({req, res})
+    // refresh sessions if it expired
+    await supabase.auth.getSession();
+    return res;
+}
