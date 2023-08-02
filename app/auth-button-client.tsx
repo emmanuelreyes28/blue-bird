0bc0279421e303d912855b29331aea18b88fe49b
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2023-08-01 17:17:28 -0700

    dynamically render ui based on user session with ssr in next.js client components

---
+++ app/auth-button-client.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import {
+  Session,
+  createClientComponentClient,
+} from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+
+export default function AuthButtonClient({
+  session,
+}: {
+  session: Session | null;
+}) {
+  const supabase = createClientComponentClient();
+  const router = useRouter();
+
+  const handleSignIn = async () => {
+    //signin with OAuth
+    await supabase.auth.signInWithOAuth({
+      provider: "github",
+      // once auth flow has been completed the user will be redirected
+      options: {
+        redirectTo: "http://localhost:3000/auth/callback",
+      },
+    });
+  };
+
+  const handleSignOut = async () => {
+    await supabase.auth.signOut();
+    // refreshing the route will stop showing tweets bc the user is not authenticated
+    router.refresh();
+  };
+
+  // only show login button if user is logged out and vice versa using session
+  return session ? (
+    <button onClick={handleSignOut}>Logout</button>
+  ) : (
+    <button onClick={handleSignIn}>Login</button>
+  );
+}
