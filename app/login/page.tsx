befa317d184b6570b898f378f32dbb81e4bf1628
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2023-08-01 17:25:54 -0700

    implement protected routes for authenticated users

---
+++ app/login/page.tsx
@@ -0,0 +1,20 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import AuthButtonClient from "../auth-button-client";
+
+export default async function Login() {
+  const supabase = createServerComponentClient({ cookies });
+
+  // get user current session if there is one
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  // if there is a session then redirect to landing page
+  if (session) {
+    redirect("/");
+  }
+
+  return <AuthButtonClient session={session} />;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,19 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import Image from "next/image";
 import AuthButtonServer from "./auth-button-server";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies });
+  // get user current session if there is one
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  // if no session meaning user is not logged in then redirect to login page
+  if (!session) {
+    redirect("/login");
+  }
   const { data: tweets } = await supabase.from("tweets").select();
 
   return (
